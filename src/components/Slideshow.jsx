224068b57c0faf799d5a28f09a1a7bbe58e47744
bngcwayi <bngcwayi@gmail.com>
2024-02-08 13:54:53 +0200

    images are displayed

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -4,7 +4,9 @@ export const Slideshow = ({ data }) => {
   return (
     <div>
       {data.map((item, idx) => {
-        return <img src={item.src} alt={item.alt} key={idx} />;
+        return (
+          <img src={item.src} alt={item.alt} width={item.width} key={idx} />
+        );
       })}
     </div>
   );
