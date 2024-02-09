e90a57525e44a19ff57ad2d9065681ab2d731eef
bngcwayi <bngcwayi@gmail.com>
2024-02-09 11:54:41 +0200

    added functionality to the arrows and indicators

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,10 +1,19 @@
+import React, { useState } from "react";
 import "./Slideshow.css";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 export const Slideshow = ({ data }) => {
+  const [Slides, setSlide] = useState(0);
+
+  const nextSlide = () => {
+    setSlide(Slides === data.length - 1 ? 0 : Slides + 1);
+  };
+  const prevSlide = () => {
+    setSlide(Slides === 0 ? data.length - 1 : Slides - 1);
+  };
   return (
     <div className="carousel">
-      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevSlide} />
 
       {data.map((item, idx) => {
         return (
@@ -13,15 +22,24 @@ export const Slideshow = ({ data }) => {
             alt={item.alt}
             width={item.width}
             key={idx}
-            className="slide"
+            className={Slides === idx ? "slide" : "slide slide-hidden"}
           />
         );
       })}
-      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <BsArrowRightCircleFill
+        className="arrow arrow-right"
+        onClick={nextSlide}
+      />
       <span className="indicators">
         {data.map((_, idx) => {
           return (
-            <button key={idx} onClick={null} className="indicator"></button>
+            <button
+              key={idx}
+              onClick={() => setSlide(idx)}
+              className={
+                Slides === idx ? "indicator" : "indicator indicator-inactive"
+              }
+            ></button>
           );
         })}
       </span>
