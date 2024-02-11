b4cac788f932f6fc734ecc65ecca1a654ea40420
bngcwayi <122809309+bngcwayi@users.noreply.github.com>
2024-02-11 18:33:20 +0200

    refining the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,11 @@ function Navbar() {
   return (
     <div>
       <nav>
+        <input type="checkbox" id="check" />
+        <label htmlFor="check">
+          <i class="fa-solid fa-bars" id="btn"></i>
+          <i class="fa-solid fa-times" id="cancel"></i>
+        </label>
         <ul>
           <li>
             <a href="#">Home </a>
