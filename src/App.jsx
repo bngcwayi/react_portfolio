fb925e18f6eded0527a3bbb722bc89b7f77344b4
bngcwayi <bngcwayi@gmail.com>
2024-02-12 13:30:58 +0200

    the structure of the form is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-
 import Navbar from "./components/Navbar";
 import Logo from "./components/Logo";
-import "./App.css";
 import { Slideshow } from "./components/Slideshow";
 import { Slides } from "./data/CarouselData.json";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       <Logo />
       <Navbar />
       <Slideshow data={Slides} />
+      <About />
+      <Projects />
+      <Contact />
     </div>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,39 @@
 import "./Contact.css";
 
 function Contact() {
-  return <div></div>;
+  return (
+    <div className="form-container">
+      <h3>Get in Touch</h3>
+      <form action="">
+        <div className="Name">
+          <label htmlFor="name">
+            <input type="text" name="name" placeholder="Name" required />
+          </label>
+        </div>
+        <div classNme="email">
+          <label htmlFor="email">
+            <input type="email" placeholder="Email" required />
+          </label>
+        </div>
+
+        <div className="textarea">
+          <label htmlFor="textarea">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Message me.."
+              required
+            ></textarea>
+          </label>
+        </div>
+
+        <div className="submit-button">
+          <button type="submit">Send email</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 export default Contact;
