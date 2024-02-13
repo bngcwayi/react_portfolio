ee702f3bc4bf75ea0155607b976af565419d2448
bngcwayi <bngcwayi@gmail.com>
2024-02-13 16:21:50 +0200

    create the project page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Slides } from "./data/CarouselData.json";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import ProjectPage from "./components/ProjectPage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <ProjectPage />
     </div>
   );
 }
--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,6 +1,26 @@
 import "./ProjectPage.css";
 
 function ProjectPage() {
-  return <div className="projectPage"></div>;
+  return (
+    <div className="projectPage">
+      <ul className="cardContainer">
+        <li className="projectNaruto">
+          <img
+            src={"/src/images/Naruto.png"}
+            alt="Top-logo"
+            width="300"
+            height=""
+          />
+          <h3>
+            <a href="">Tribute Page:Naruto</a>
+          </h3>
+          <p>
+            This tribute page honors the ninja's resilience, showcasing how he
+            triumphed over adversity and never wavered in pursuing his dreams.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
 }
 export default ProjectPage;
