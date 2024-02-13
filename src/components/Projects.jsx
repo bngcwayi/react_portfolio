f0b84c7dc2032038cadd577ec08251ed6c912192
bngcwayi <bngcwayi@gmail.com>
2024-02-13 10:04:14 +0200

    Added 3 unordered lists to showcase the 3 projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,11 +12,47 @@ function Projects() {
             width="300"
             height=""
           />
-          <h3>Tribute Page:Naruto</h3>
+          <h3>
+            <a href="">Tribute Page:Naruto</a>
+          </h3>
+          <p>
+            This tribute page honors the ninja's resilience, showcasing how he
+            triumphed over adversity and never wavered in pursuing his dreams.
+          </p>
         </li>
 
-        <li className="project02">Project02</li>
-        <li className="project03">Project03</li>
+        <li className="project02">
+          Project02
+          <img
+            src={"/src/images/Naruto.png"}
+            alt="Top-logo"
+            width="300"
+            height=""
+          />
+          <h3>
+            <a href="">Tribute Page:Naruto</a>
+          </h3>
+          <p>
+            This tribute page honors the ninja's resilience, showcasing how he
+            triumphed over adversity and never wavered in pursuing his dreams.
+          </p>
+        </li>
+        <li className="project03">
+          Project03
+          <img
+            src={"/src/images/Naruto.png"}
+            alt="Top-logo"
+            width="300"
+            height=""
+          />
+          <h3>
+            <a href="">Tribute Page:Naruto</a>
+          </h3>
+          <p>
+            This tribute page honors the ninja's resilience, showcasing how he
+            triumphed over adversity and never wavered in pursuing his dreams.
+          </p>
+        </li>
       </ul>
     </div>
   );
