1d3154a69430a3e00364743997e616318593b8b9
bngcwayi <122809309+bngcwayi@users.noreply.github.com>
2024-02-13 23:15:41 +0200

    structure of the project page is complete

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -5,19 +5,22 @@ function ProjectPage() {
     <div className="projectPage">
       <ul className="cardContainer">
         <li className="projectNaruto">
-          <img
-            src={"/src/images/Naruto.png"}
-            alt="Top-logo"
-            width="300"
-            height=""
-          />
           <h3>
             <a href="">Tribute Page:Naruto</a>
           </h3>
+          <button className="launch-btn">Launch</button>
+          <img
+            src={"/src/images/Naruto.png"}
+            alt="Top-logo"
+            width="250"
+            height=""
+          />
+
           <p>
             This tribute page honors the ninja's resilience, showcasing how he
             triumphed over adversity and never wavered in pursuing his dreams.
           </p>
+          <button className="launch-btn">Learn more + github icon</button>
         </li>
       </ul>
     </div>
