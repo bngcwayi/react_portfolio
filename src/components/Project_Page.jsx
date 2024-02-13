39a3c0de0be978657757aaa899f807c589cd0e2f
bngcwayi <bngcwayi@gmail.com>
2024-02-13 09:54:15 +0200

    started creating a page listing the projects, aiming for card like design

--- src/components/Project_Page.jsx
+++ src/components/Project_Page.jsx
@@ -1,6 +1,6 @@
 import "./Project_Page.css";
 
 function Project_Page() {
-  return <div></div>;
+  return <div className="projectPage"></div>;
 }
 export default Project_Page;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,24 @@
 import "./Projects.css";
 
 function Projects() {
-  return <div></div>;
+  return (
+    <div className="projectContainer">
+      <ul className="cards-wrapper">
+        <li className="project01">
+          Project01
+          <img
+            src={"/src/images/Naruto.png"}
+            alt="Top-logo"
+            width="300"
+            height=""
+          />
+          <h3>Tribute Page:Naruto</h3>
+        </li>
+
+        <li className="project02">Project02</li>
+        <li className="project03">Project03</li>
+      </ul>
+    </div>
+  );
 }
 export default Projects;
