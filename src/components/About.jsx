0cc05996e2b2c80ab7ac9ce2c713d3e6f881ac77
bngcwayi <bngcwayi@gmail.com>
2024-02-14 16:36:30 +0200

    busy with grid for the skillls section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,16 +19,95 @@ function About() {
         </p>
       </div>
       <div className="Skills">
-        <div className="item1">1</div>
-        <div className="item2">2</div>
-        <div className="item3">3</div>
-        <div className="item4">4</div>
-        <div className="item5">5</div>
-        <div className="item6">6</div>
-        <div className="item7">7</div>
-        <div className="item8">8</div>
-        <div className="item9">9</div>
-        <div className="item10">10</div>
+        <div className="item1">
+          <img
+            src={"/src/images/Logo_v2.png"}
+            alt="Top-logo"
+            width="150"
+            height="100"
+          />
+        </div>
+        <div className="item2">
+          {" "}
+          <img
+            src={"/src/images/Logo_v2.png"}
+            alt="Top-logo"
+            width="150"
+            height="100"
+          />
+        </div>
+        <div className="item3">
+          {" "}
+          <img
+            src={"/src/images/Logo_v2.png"}
+            alt="Top-logo"
+            width="150"
+            height="100"
+          />
+        </div>
+        <div className="item4">
+          {" "}
+          <img
+            src={"/src/images/Logo_v2.png"}
+            alt="Top-logo"
+            width="150"
+            height="100"
+          />
+        </div>
+        <div className="item5">
+          {" "}
+          <img
+            src={"/src/images/Logo_v2.png"}
+            alt="Top-logo"
+            width="150"
+            height="100"
+          />
+        </div>
+        <div className="item6">
+          {" "}
+          <img
+            src={"/src/images/Logo_v2.png"}
+            alt="Top-logo"
+            width="150"
+            height="100"
+          />
+        </div>
+        <div className="item7">
+          {" "}
+          <img
+            src={"/src/images/Logo_v2.png"}
+            alt="Top-logo"
+            width="150"
+            height="100"
+          />
+        </div>
+        <div className="item8">
+          {" "}
+          <img
+            src={"/src/images/Logo_v2.png"}
+            alt="Top-logo"
+            width="150"
+            height="100"
+          />
+        </div>
+        <div className="item9">
+          {" "}
+          <img
+            src={"/src/images/Logo_v2.png"}
+            alt="Top-logo"
+            width="150"
+            height="100"
+          />
+        </div>
+        <div className="item10">
+          {" "}
+          <img
+            src={"/src/images/Logo_v2.png"}
+            alt="Top-logo"
+            width="150"
+            height="100"
+          />
+        </div>
       </div>
     </div>
   );
