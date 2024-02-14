5b0aa90d3ddfceebad07b55e4ab1e1c8f94b923b
bngcwayi <bngcwayi@gmail.com>
2024-02-14 09:26:11 +0200

    added styling to the launch button

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -5,9 +5,7 @@ function ProjectPage() {
     <div className="projectPage">
       <ul className="cardContainer">
         <li className="projectNaruto">
-          <h3>
-            <a href="">Tribute Page:Naruto</a>
-          </h3>
+          <h3>Tribute Page:Naruto</h3>
           <button className="launch-btn">Launch</button>
           <img
             src={"/src/images/Naruto.png"}
@@ -19,7 +17,7 @@ function ProjectPage() {
             This tribute page honors the ninja's resilience, showcasing how he
             triumphed over adversity and never wavered in pursuing his dreams.
           </p>
-          <button className="launch-btn">Learn more + github icon</button>
+          <button className="LearnMore-btn">Learn more + github icon</button>
         </li>
       </ul>
     </div>
